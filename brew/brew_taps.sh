d614008a558206e0733622b8e941e1a73e34fa79
Nilesh Parkhe <parkhenilesh@outlook.com>
2024-04-06 15:31:35 +0530

    Added new Brew Tap to install fonts

--- brew/brew_taps.sh
+++ brew/brew_taps.sh
@@ -5,3 +5,17 @@ brew tap rusty-ferris-club/tap # Needed for shellclear
 
 # Tap for Hashicorp Vault
 brew tap hashicorp/tap
+
+# Tap the Homebrew font cask repository if not already tapped
+brew tap | grep -q "^homebrew/cask-fonts$" || brew tap homebrew/cask-fonts
+
+# Define the font name
+font_name="font-source-code-pro"
+
+# Check if the font is already installed
+if brew list --cask | grep -q "^$font_name\$"; then
+    echo "$font_name is already installed. Skipping..."
+else
+    echo "Installing $font_name..."
+    brew install --cask "$font_name"
+fi
