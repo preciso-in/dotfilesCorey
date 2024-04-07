026f684a16c14a7cecfdad775b4f9478541e978b
Nilesh Parkhe <parkhenilesh@outlook.com>
2024-04-07 20:49:39 +0530

    Fix git credentials commit

--- git-setup.sh
+++ git-setup.sh
@@ -5,3 +5,7 @@ read git_user_name
 # Git config email
 echo "Please enter your EMAIL for Git configuration:"
 read git_user_email
+
+# Set my git credentials
+$(brew --prefix)/bin/git config --global user.name "$git_user_name"
+$(brew --prefix)/bin/git config --global user.email "$git_user_email"
