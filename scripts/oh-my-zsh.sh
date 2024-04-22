50fa7db9213605a1c6b8feb1d6f16a89700562ad
Nilesh Parkhe <parkhenilesh@gmail.com>
2024-04-22 12:26:14 +0530

    Fix bug

--- scripts/oh-my-zsh.sh
+++ scripts/oh-my-zsh.sh
@@ -1,3 +1,5 @@
-cd ../git_repos && git clone https://github.com/ohmyzsh/ohmyzsh.git
+mkdir git_repos
+
+cd git_repos && git clone https://github.com/ohmyzsh/ohmyzsh.git
 
 cd $HOME && ln -s $PWD/git_repos/oh-my-zsh .oh-my-zsh
