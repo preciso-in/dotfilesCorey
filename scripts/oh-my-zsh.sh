9a08322315e1be33b1fd2897ce76973af48836b4
Nilesh Parkhe <parkhenilesh@gmail.com>
2024-04-22 13:43:44 +0530

    fix for ohmyzsh

--- scripts/oh-my-zsh.sh
+++ scripts/oh-my-zsh.sh
@@ -7,4 +7,8 @@ fi
 mkdir $DIRECTORY
 cd $DIRECTORY && git clone https://github.com/ohmyzsh/ohmyzsh.git
 
-cd $HOME && ln -sf $PWD/git_repos/oh-my-zsh .oh-my-zsh
+pushd $HOME
+echo $PWD
+exit
+git_repos/oh-my-zsh .oh-my-zsh
+popd
