a3df4f514f069021cc5426b52cd13680521bf8b3
Nilesh Parkhe <parkhenilesh@outlook.com>
2024-04-24 16:07:56 +0530

    Add Yabai & SKHD config

--- scripts/tidy-up.sh
+++ scripts/tidy-up.sh
@@ -1,1 +1,10 @@
 vault -autocomplete-install 2>/dev/null
+
+# Copy Yabai and skhd configs to .config
+if ! [ -d $HOME/.config ]; then
+  mkdir $HOME/.config
+fi
+SCRIPT_DIR=$(dirname $(readlink -f "$0"))
+DOTFILES_FOLDER=$(dirname $SCRIPT_DIR)
+ln -sf $DOTFILES_FOLDER/settings/yabai $HOME/.config/yabai
+ln -sf $DOTFILES_FOLDER/settings/skhd $HOME/.config/skhd
