7995ffb1db35d4540ef818e0c5caba47e8a9ab42
Nilesh Parkhe <parkhenilesh@outlook.com>
2024-04-28 13:57:19 +0530

    Create hosts file instead of copying from repo
    - Downloads latest hosts file from Steven Black Project
    - Combines downloaded ignore entries with local custom_hosts
    - Copies to /etc

--- scripts/macOS.sh
+++ scripts/macOS.sh
@@ -17,7 +17,28 @@ sudo mdutil -i off -d /Volumes/PortableSSD/
 # Add Adblock hosts file to OS
 SCRIPT_DIR=$(dirname $(readlink -f "$0"))
 DOTFILES_DIR=$(dirname $SCRIPT_DIR)
-echo $DOTFILES_DIR
+
+unlink /etc/hosts
+sudo rm /etc/hosts
+
+pushd ../MacOS
+# Create hosts file from Steven Black's project
+sudo docker run \
+  --pull always \
+  --platform linux/x86_64 \
+  --rm \
+  -it \
+  -v $DOTFILES_DIR/MacOS/hosts:/etc/hosts \
+  ghcr.io/stevenblack/hosts:latest updateHostsFile.py \
+  --auto \
+  --replace \
+  --extensions gambling porn fakenews social
+
+cat "" >>hosts
+cat custom_hosts >>hosts
+
 sudo cp $DOTFILES_DIR/MacOS/hosts /etc/hosts
+popd
+
 unset DOTFILES_DIR
 unset SCRIPT_DIR
