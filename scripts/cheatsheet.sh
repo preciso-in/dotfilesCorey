bfd47d61aa67e8403e150925ca7cafe671b0f378
Nilesh Parkhe <parkhenilesh@outlook.com>
2024-04-28 20:57:06 +0530

    Display Sorted Cheatsheet Options

--- scripts/cheatsheet.sh
+++ scripts/cheatsheet.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env zsh
 
-languages=$(echo "typescript javascript docker tmux mongo mongod go atuin fzf mysql psql git git-log python autojump" | tr " " "\n")
-core_utils=$(echo "awk bash find jq git sed xargs" | tr " " "\n")
+languages=$(echo "typescript javascript docker tmux mongo mongod go atuin fzf mysql psql git git-log python autojump" | tr " " "\n" | sort)
+core_utils=$(echo "awk bash find jq git sed xargs" | tr " " "\n" | sort)
 selected=$(echo -e "$languages\n$core_utils" | fzf)
 echo "Query: "
 read query
