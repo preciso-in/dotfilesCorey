67f7b0389c8c9023dd0f9f1ab567968256b53b07
Nilesh Parkhe <parkhenilesh@outlook.com>
2024-06-08 08:52:58 +0530

    Add background app "Synergy" on login

--- scripts/start-services.sh
+++ scripts/start-services.sh
@@ -3,3 +3,8 @@
 # Start yabai and skhd services
 yabai --start-service
 skhd --start-service
+
+# Start apps in background
+# -a is for opening apps stored in the /Applications folder. These apps cannot be opened without -a
+open -a synergy
+
