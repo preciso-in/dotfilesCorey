f41a1c6003372d4bfcacb0360d68ebb33e0cc301
Nilesh Parkhe <parkhenilesh@outlook.com>
2024-09-18 09:58:59 +0530

    Add Browser Extensions to list

--- settings/browser_extensions.sh
+++ settings/browser_extensions.sh
@@ -4,6 +4,12 @@ list_extensions=(
     "vimium"
     "GoFullPage"
     "Enhancer for Youtube"
+    "Dev extensions reload - michaelonikienko - Alt + Z to reload extensions"
+    "Bulk URL Opener - Meetanshi"
+    "Copy URLs - melanto"
+    "GoFullPage"
+    "JSONVue"
+    "Copy All URLs"
 )
 
 echo "Add following extensions to Chrome, Opera and Arc"
