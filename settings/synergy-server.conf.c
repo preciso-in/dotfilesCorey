a9b05d7c7009600e320566193d64611c99ffc637
Nilesh Parkhe <parkhenilesh@outlook.com>
2024-10-13 11:05:30 +0530

    Add Symless Synergy Config

---
+++ settings/synergy-server.conf.c
@@ -0,0 +1,37 @@
+section: screens
+	Neo:
+		halfDuplexCapsLock = false
+		halfDuplexNumLock = false
+		halfDuplexScrollLock = false
+		xtestIsXineramaUnaware = false
+		switchCorners = none 
+		switchCornerSize = 0
+	Surface:
+		halfDuplexCapsLock = false
+		halfDuplexNumLock = false
+		halfDuplexScrollLock = false
+		xtestIsXineramaUnaware = false
+		switchCorners = none 
+		switchCornerSize = 0
+end
+
+section: aliases
+end
+
+section: links
+	Neo:
+		right = Surface
+	Surface:
+		left = Neo
+end
+
+section: options
+	relativeMouseMoves = false
+	win32KeepForeground = false
+	disableLockToScreen = false
+	clipboardSharing = true
+	clipboardSharingSize = 3072
+	switchCorners = none 
+	switchCornerSize = 0
+end
+
