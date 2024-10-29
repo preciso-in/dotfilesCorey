ad75710066c479cc8d4bab5187ef450729e0e92d
Nilesh Parkhe <parkhenilesh@outlook.com>
2024-10-29 09:49:13 +0530

    Update ZSh Remote Script to show progress

--- scripts/remote_terminal_config.sh
+++ scripts/remote_terminal_config.sh
@@ -8,31 +8,47 @@
 
 {
   # Install thefuck plugin code
+  echo -e '\n\nInstalling thefuck - 1/6'
+  echo -e 'Pending Installs - neofetch, lsd, Tree, ZSh & OhMyZSh'
   sudo apt update
   sudo apt install python3-dev python3-pip python3-setuptools -y
   pip3 install thefuck --user
 
   # Install neofetch
   # Get Distro details
+  echo -e '\n\nInstalling neofetch - 2/6'
+  echo -e 'Pending Installs - lsd, Tree, ZSh & OhMyZSh'
   sudo apt install neofetch -y
 
   # Better ls - Show icons for file/dir types
+  echo -e '\n\nInstalling lsd - 3/6'
+  echo -e 'Pending Installs - Tree, ZSh & OhMyZSh'
   snap install lsd
 
+  # Installing tree command for bash
+  echo -e '\n\nInstalling tree - 4/6'
+  echo -e 'Pending Installs ZSh & OhMyZSh'
+  snap install tree
+
   # Install ZSh & OhMyZSh
+  echo -e '\n\nInstalling ZSh & OhMyZSh - 5/6'
   sudo apt install zsh -y
   sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
   # Install ZSh-AutoSuggestions & ZSh-Syntax-Highlighting & You-Should-Use
+  echo -e '\n\nInstalling Auto-suggestions, syntax-highlighting & YouShouldUse - 6/6'
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
   git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
 
   # Edit .zshrc - Add plugins, Change Default Editor, Add spaces to prompt
+  echo -e '\n\nAdding plugins and entries to .zshrc'
   sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting kubectl you-should-use)/' $HOME/.zshrc
   echo -e "\nexport EDITOR=vi" >>$HOME/.zshrc
   export PATH=$PATH:/root/.local/bin
   eval $(thefuck --alias)
+
+  echo -e '\n\nCreating Alias'
   echo -e "\nalias kex='kubectl explain'" >>$HOME/.zshrc
 
   echo -e "\nalias kcrns='kubectl create namespace' " >>$HOME/.zshrc
@@ -48,6 +64,7 @@
 }' >>.zshrc
 
   # Set configuration for vim
+  echo -e '\n\nConfiguring Vim'
   cat <<EOF >.vimrc
 "Remap ESC to jk and JK
 inoremap jk <ESC>
@@ -72,6 +89,7 @@ set expandtab
 EOF
 
   # Update yq version
+  echo -e "\n\nUpdating yq version"
   sudo apt remove yq -y && snap install yq
 
   # Run ZSh
